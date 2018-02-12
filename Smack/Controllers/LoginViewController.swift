1f3f2ec137cc8a96ae79ad26716e0b98415e5f46
jhubbard <justin@Justins-MacBook-Pro.local>
2018-02-12 14:15:25 -0800

    Lesson 4

---
+++ Smack/Controllers/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  Smack
+//
+//  Created by Justin Hubbard on 2/12/18.
+//  Copyright © 2018 Justin Hubbard. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Justin Hubbard on 2/5/18.
+//  Copyright © 2018 Justin Hubbard. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
